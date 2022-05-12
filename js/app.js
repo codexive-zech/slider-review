fe791f0c3312a286ad057eba3b8c399671028e19
Codexive_zech <zechariahhounwanou@gmail.com>
2022-05-12 21:37:45 +0100

    added the functinality that does the next and prev btn click and event listener on the prev and next button
    got the position classes that were dynamically created into the HTML, then check if next revies is null so as to go back to the first review child, then remove them all the classes added, then inter change and added a new class to them all active got last - last got next - next got active

--- js/app.js
+++ js/app.js
@@ -12,8 +12,6 @@ const displayPersonReview =
     .map((person, reviewIndex) => {
       // destructed the properties form the people object which is need to be displayed in the HTML
       const { name, job: title, text, img } = person;
-      //  dynamically return and display each single slider and added the destructured properties into the need HTML space
-
       //   getting all index position of the people object array and by default give it to position next
       let position = 'next';
       //   checking to see if the index of people object array is 0
@@ -26,6 +24,7 @@ const displayPersonReview =
         //   let that review have the position of last
         position = 'last';
       }
+      //  dynamically return and display each single slider and added the destructured properties into the need HTML space
       return `<!-- Single slide -->
         <article class="slide ${position}">
           <!-- img -->
@@ -52,3 +51,39 @@ const displayPersonReview =
     .join('');
 // dynamically inserting each person review in the HTML
 sliderContainer.innerHTML = displayPersonReview;
+
+// having a func expression that take in most functionality for prev and next button
+const startSlider = (type) => {
+  // selecting all three classes that was added dynamically into the HTML using the position of the review index
+  const active = getElement('.active');
+  const last = getElement('.last');
+  // using let since value changes getting the next review after the class of active
+  let next = active.nextElementSibling;
+  // if the is not more data for next in the people object array (last person review in the people data) i.e when next is detail does not exist
+  if (!next) {
+    // let next take in a new value of the first review detail in the people object array
+    next = sliderContainer.firstElementChild;
+  }
+
+  // removing each classes that was added dynamically into the HTML from the position of the review index and pass in the array bracket because they are multiple classes
+  active.classList.remove(['active']);
+  last.classList.remove(['last']);
+  next.classList.remove(['next']);
+
+  // adding each classes to elements by their side, swapping classes
+  active.classList.add('last');
+  last.classList.add('next');
+  next.classList.add('active');
+};
+
+// listening for a click event on the next button
+nextBtn.addEventListener('click', () => {
+  // invoking the start slider func
+  startSlider();
+});
+
+// listening for a click event on the prev button
+prevBtn.addEventListener('click', () => {
+  // invoking the start slider func
+  startSlider('prev');
+});
